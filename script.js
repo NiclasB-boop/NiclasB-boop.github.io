9a750d426f40152936d3406e2ea9ff2ae151b370
NiclasB-boop <niclas.barthel@mrge.lernsax.de>
2025-04-17 16:39:56 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
-function div-to-text() {
-    document.getElementById("html-text").value = document.getElementById("text")
+
+function Passwort() {
+    const Gross = document.getElementById("Gross").checked
+    const Klein = document.getElementById("Klein").checked
+    const Zahl = document.getElementById("Zahl").checked
+    const Sonder = document.getElementById("Sonder").checked
+    const Kleinbuchstaben = "abcdefghijklmnopqrstuvwxyz"
+    const Grossbuchstaben = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const Zahlen = "1234567890"
+    const Sonderzeichen = "!\"§$%&/()=?`²³{[]}\\^°@€µ|<>"
+    const Laenge = document.getElementById("Laenge").value
+    let chars = ""
+
+    if (Gross) chars += Grossbuchstaben
+    if (Klein) chars += Kleinbuchstaben
+    if (Zahl) chars += Zahlen
+    if (Sonder) chars += Sonderzeichen
+    
+    let passwort = ""
+    for (let i =0; i < Laenge; i++) {
+        passwort += chars[Math.floor(Math.random()*chars.length)]
+    }
+    document.getElementById("Passwort").innerText = passwort
 }
